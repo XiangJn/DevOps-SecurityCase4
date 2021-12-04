7b9b362826de58765f1ada236aab020e853d1905
r0744382 <r0744382@student.thomasmore.be>
2021-12-05 00:38:20 +0100

    model + azure db

---
+++ DevOps-SecurityCase4/Model/Anime.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevOps_SecurityCase4.Model
+{
+    class Anime : BaseModel
+    {
+        private int id;
+        private string title;
+        private string status;
+ 
+
+
+
+        public int ID
+        {
+            get { return id; }
+
+            set
+            {
+                id = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public string Title
+        {
+            get { return title; }
+
+            set
+            {
+                title = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public string Status
+        {
+            get { return status; }
+
+            set
+            {
+                status = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+
+
+
+    }
+}
---
+++ DevOps-SecurityCase4/Model/AnimeDataService.cs
@@ -0,0 +1,50 @@
+﻿using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+using Dapper;
+using System.Collections.Generic;
+
+namespace DevOps_SecurityCase4.Model
+{
+    class AnimeDataService
+    {
+        private static string connectionString =
+                ConfigurationManager.ConnectionStrings["azure"].ConnectionString;
+        private static IDbConnection db = new SqlConnection(connectionString);
+
+        public List<Anime> GetHighscore()
+        {
+            string sql = "Select * from Anime order by Turns";
+            return (List<Anime>)db.Query<Anime>(sql);
+        }
+
+        public void UpdateHighscore(Anime anime)
+        {
+            string sql = "Update Anime set playerId = @playerId, turns = @turns where id = @id";
+            db.Execute(sql, new
+            {
+                anime.PlayerId,
+                anime.Turns,
+                anime.ID
+            });
+        }
+
+        public void InsertHighscore(Anime anime)
+        {
+            string sql = "Insert into Anime (playerId, turns) values (@playerId, @turns)";
+            db.Execute(sql, new
+            {
+                anime.PlayerId,
+                anime.Turns
+            });
+        }
+
+
+        public void DeleteHighscore(Anime anime)
+        {
+            string sql = "Delete Anime where id = @id";
+            db.Execute(sql, new { anime.ID });
+        }
+
+    }
+}
